2a97e720a9879d6bb3ba1fe79173c451367cf30d
dr-dolce14 <dolcemascop1@gmail.com>
2020-07-19 16:08:23 -0400

    got everything working including timer but cannot get alert to go away and restart game after no more moves left

--- index.js
+++ index.js
@@ -1,6 +1,67 @@
+console.log("WORKING HARD!")
 document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
-
+  
+  
   // ADD CODE HERE!
-})
+  const moves = document.getElementById('moves-container')
+  const moveButton = document.getElementById('move-button')
+  let moving = false
+
+  
+  document.addEventListener("keydown", function(e) {
+    const direction = document.createElement("li")
+    if (e.keyCode === 37) {
+    direction.textContent = "left"
+    moves.append(direction)
+    }
+    if (e.keyCode === 38) {
+    direction.textContent = "up"
+    moves.append(direction)
+    }
+    if (e.keyCode === 39) {
+    direction.textContent = "right"
+    moves.append(direction)
+    }
+    if (e.keyCode === 40) {
+    direction.textContent = "down"
+    moves.append(direction)
+    }
+    if (e.key === "Backspace") {
+      if (moves.lastChild) {
+        moves.lastChild.remove()
+      } else {
+        alert("nothing to delete!")
+      }
+      
+    }
+  })
+  
+
+  moveButton.addEventListener("click", function(e) {
+    if (moving === false) {
+    setInterval(() => moveRobot(), 500)
+    }
+  })
+
+
+    function moveRobot() {
+        const firstMove = moves.querySelector('li')
+        if (firstMove) {
+        const dir = firstMove.textContent
+        move(dir)
+        firstMove.remove() 
+        moving = true
+       } else {
+         alert("no more directions!")
+         moving = true
+        }
+       
+  }
+
+  // cannot get the alert to go away and restart the game
+  
+  }) // DOMContentLoaded end
+
+
